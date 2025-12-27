3caec7c86b6f942c42ac76bdbc0cd4026b0cb601
Monisha Singh <monisha.singh06@gmail.com>
2025-12-27 23:01:59 +0530

    Added navigation bar, routing completed, background of homepage is done

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,24 @@
-import { Text, View } from "react-native";
+import SearchBar from "@/components/SearchBar";
+import { Image, ScrollView, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-light-200">
-        Welcome to Nativewind!
-      </Text>
+    <View className="flex-1 bg-primary">
+      <Image
+        source={require("@/assets/images/bg.png")}
+        className="w-full h-full z-0 absolute"
+      />
+      <ScrollView
+        className="flex-1 px-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image
+          source={require("@/assets/icons/logo.png")}
+          className="w-12 h-10 mt-20 mb-8 mx-auto"
+        />
+        <SearchBar />
+      </ScrollView>
     </View>
   );
 }
