906842ff09d124af2fd8c5b0a2b7de9c7a36e240
Monisha Singh <monisha.singh06@gmail.com>
2025-12-30 14:59:51 +0530

    Search functionality implemented

--- app/movie/[id].tsx
+++ app/movie/[id].tsx
@@ -1,10 +1,85 @@
+import { useLocalSearchParams } from "expo-router";
 import React from "react";
-import { Text, View } from "react-native";
+import { Image, ScrollView, Text, View } from "react-native";
 
 const movieDetails = () => {
+  const { id, poster_path, title, date, star, popularity, overview, geners } =
+    useLocalSearchParams<{
+      id: string;
+      poster_path: string;
+      title: string;
+      date: string;
+      star: any;
+      popularity: any;
+      overview: string;
+      geners: any;
+    }>();
+
   return (
-    <View>
-      <Text>movieDetails</Text>
+    <View className="bg-primary flex-1">
+      <ScrollView>
+        <Image
+          source={{
+            uri: `https://image.tmdb.org/t/p/w500${poster_path}`,
+          }}
+          style={{ width: "100%", height: 600, marginBottom: 20 }}
+        />
+
+        <View className="mx-5">
+          <Text className="text-white text-2xl font-bold">{title}</Text>
+          <Text style={{ color: "#c7c7c7", marginTop: 5 }}>
+            {date.split("-")[0]}
+          </Text>
+
+          <View
+            className="inline-flex flex-row items-end p-3 pt-2 bg-[#1e1e1e] rounded-lg mt-5"
+            style={{ maxWidth: "35%" }}
+          >
+            <Image
+              source={require("@/assets/icons/star.png")}
+              className="w-5 h-5 "
+              style={{ tintColor: "#f5c518", marginRight: 5, marginBottom: 1 }}
+            />
+            <Text className="text-white text-lg mb-[-2]">{star}</Text>
+            <Text className="text-['#c7c7c7'] text-md">
+              /10 ({popularity}K)
+            </Text>
+          </View>
+
+          <View style={{ marginTop: 40 }}>
+            <Text className="text-['#c7c7c7'] text-md font-normal mb-2">
+              Overview
+            </Text>
+            <Text className="text-white text-lg font-light">{overview}</Text>
+          </View>
+
+          <View className="flex flex-row justify-between w-[80%]">
+            <View style={{ marginTop: 40 }}>
+              <Text className="text-['#c7c7c7'] text-md font-normal mb-1">
+                Release Date
+              </Text>
+              <Text className="text-['#a8b5db'] text-lg font-light">
+                {date} (Worldwide)
+              </Text>
+            </View>
+            <View style={{ marginTop: 40 }}>
+              <Text className="text-['#c7c7c7'] text-md font-normal mb-1">
+                Status
+              </Text>
+              <Text className="text-['#a8b5db'] text-lg font-light">
+                Released
+              </Text>
+            </View>
+          </View>
+
+          <View style={{ marginTop: 40 }}>
+            <Text className="text-['#c7c7c7'] text-md font-normal mb-2">
+              Generes
+            </Text>
+            <Text className="text-white text-lg font-light">{geners}</Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 };
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,6 +1,16 @@
 import { Image, TextInput, View } from "react-native";
 
-export default function SearchBar() {
+export default function SearchBar({
+  disabled = false,
+  autoFocus = false,
+  query,
+  setQuery,
+}: {
+  disabled?: boolean;
+  autoFocus?: boolean;
+  query?: string;
+  setQuery?: (value: string) => void;
+}) {
   return (
     <View className="flex flex-row items-center mt-8 mx-auto z-10 w-full p-5 justify-center gap-6 relative">
       <Image
@@ -10,12 +20,14 @@ export default function SearchBar() {
       />
       <TextInput
         onPress={() => {}}
+        editable={!disabled}
+        autoFocus={autoFocus}
         placeholder="Search through 300+ movies online"
-        value=""
-        onChangeText={() => {}}
+        value={query}
+        onChangeText={(text) => setQuery && setQuery(text)}
         placeholderTextColor={"#a8b5db"}
         style={{ fontSize: 16 }}
-        className="flex-1 ml-20 text-semibold text-base font-medium"
+        className="flex-1 ml-20 text-semibold text-white text-base font-medium"
       />
     </View>
   );
